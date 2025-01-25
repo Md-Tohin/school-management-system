98ba3434b72b1eca584fc45ce6271ce5dd5167a9
Md-Tohin <mdtohin8585@gmail.com>
2025-01-25 21:37:47 +0600

    subcategory api

--- api/route/subcat.route.js
+++ api/route/subcat.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { getSubCategoryController } from "../controllers/subCategory.controller.js";
+import { addSubCategoryController, deleteSubCategoryController, getSubCategoryController, updateSubCategoryController } from "../controllers/subCategory.controller.js";
 
 const subCatRouter = Router();
 
 subCatRouter.post("/get", getSubCategoryController);
+subCatRouter.post("/create", auth, addSubCategoryController);
+subCatRouter.put("/update", auth, updateSubCategoryController);
+subCatRouter.delete("/delete", auth, deleteSubCategoryController);
+
 
 export default subCatRouter;
