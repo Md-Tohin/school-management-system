d0e176d8ec08ddb16ff6cd2383b3dc425648062a
Md-Tohin <mdtohin8585@gmail.com>
2025-01-25 15:38:28 +0600

    check connect server

--- api/index.js
+++ api/index.js
@@ -7,13 +7,13 @@ import morgan from 'morgan'
 import helmet from "helmet";
 import connectDB from "./config/connectDB.js";
 import categoryRouter from "./route/category.route.js";
-import userRouter from "./route/user.route.js";
-import uploadRouter from "./route/upload.route.js";
-import subCategoryRouter from "./route/subcategory.route.js";
-import productRouter from "./route/product.route.js";
-import cartRouter from "./route/cart.route.js";
-import addressRouter from "./route/address.route.js";
-import orderRouter from "./route/order.route.js";
+// import userRouter from "./route/user.route.js";
+// import uploadRouter from "./route/upload.route.js";
+// import subCategoryRouter from "./route/subcategory.route.js";
+// import productRouter from "./route/product.route.js";
+// import cartRouter from "./route/cart.route.js";
+// import addressRouter from "./route/address.route.js";
+// import orderRouter from "./route/order.route.js";
 
 const app = express();
 
@@ -40,14 +40,14 @@ app.get('/', (request, response) => {
  
 
 //  routes
-app.use('/api/user', userRouter)
+// app.use('/api/user', userRouter)
 app.use('/api/category', categoryRouter)
-app.use('/api/subcategory', subCategoryRouter)
-app.use('/api/product', productRouter)
-app.use('/api/file', uploadRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/address', addressRouter)
-app.use('/api/order', orderRouter)
+// app.use('/api/subcategory', subCategoryRouter)
+// app.use('/api/product', productRouter)
+// app.use('/api/file', uploadRouter)
+// app.use('/api/cart', cartRouter)
+// app.use('/api/address', addressRouter)
+// app.use('/api/order', orderRouter)
 
 connectDB().then(() => {
     app.listen(PORT, () => {
