e1b569b5aab5e11d988caa549c73ff756dac9e15
Md-Tohin <mdtohin8585@gmail.com>
2025-01-25 12:43:41 +0600

    test api

--- api/server.js
+++ api/server.js
@@ -1,1 +1,24 @@
-console.log("Server is running ");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookeParser = require('cookie-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.use(cookeParser());
+
+app.get("/test", (req, res) => {
+    res.send({id: 1, message: "Hello Bangladesh"})
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log("Server is running at port :",PORT);
+})
+
